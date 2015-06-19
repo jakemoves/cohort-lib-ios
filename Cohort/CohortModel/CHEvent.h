3802df4373b047095fd1063866abe94e4297eeab
Jacob Niedzwiecki <jake@jacob-n.com>
2015-06-19 13:32:09 -0400

    Added CHEpisode, did some other refactoring around error handling for CHEvent and assets

---
+++ Cohort/CohortModel/CHEvent.h
@@ -0,0 +1,13 @@
+//
+//  CHEvent.h
+//  Cohort
+//
+//  Created by Jacob Niedzwiecki on 2015-06-19.
+//  Copyright (c) 2015 Jacob Niedzwiecki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHEvent : NSObject
+
+@end
---
+++ Cohort/CohortModel/CHEvent.m
@@ -0,0 +1,13 @@
+//
+//  CHEvent.m
+//  Cohort
+//
+//  Created by Jacob Niedzwiecki on 2015-06-19.
+//  Copyright (c) 2015 Jacob Niedzwiecki. All rights reserved.
+//
+
+#import "CHEvent.h"
+
+@implementation CHEvent
+
+@end
