a954c3a33ae219c3c1bab58ff5ae478e842f44c7
Jacob Niedzwiecki <jake@jacob-n.com>
2015-09-14 00:06:44 -0400

    Fixed local notifications for iOS 7

--- Cohort/CHSession.m
+++ Cohort/CHSession.m
@@ -37,7 +37,10 @@
         // local notifications
         UIUserNotificationType types = UIUserNotificationTypeAlert | UIUserNotificationTypeSound;
         UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:types categories:nil]; // maybe should not live in library
-        [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings]; // maybe should not live in library
+        if([UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)){
+            //iOS 8+
+            [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings]; // maybe should not live in library
+        }
         
         // networking
         _sseClient = nil;
